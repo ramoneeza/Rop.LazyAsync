62748f3a85bd9fd77fad4ade81de043f86a6285f
Ramon Ordiales <ramon@eeza.csic.es>
2022-08-16 17:24:21 +0200

    Agregar archivos de proyecto.

---
+++ Readme.md
@@ -0,0 +1,16 @@
+﻿# Rop.LazyAsync
+
+Features
+--------
+
+Rop.LazyAsync is a implementation of Lazy<Task< T >> with Disposable and Reset option
+
+The package is published in nuget as 'Rop.LazyAsync'
+
+Usage
+-----
+
+As a Lazy<Task< T >> with Awaiter and Async Disposable
+
+ ------
+ (C)2022 Ramón Ordiales Plaza
---
+++ Rop.LazyAsync/LazyAsync.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Rop.LazyAsync
+{
+    public class LazyAsync<T> : IAsyncDisposable
+    {
+        private readonly Func<Task<T>> _factory;
+        private Lazy<Task<T>> _lazy;
+        public LazyAsync(Func<Task<T>> factory)
+        {
+            _factory = factory;
+            _lazy = new Lazy<Task<T>>(_factory);
+        }
+        public LazyAsync(Func<T> factory) : this(() => Task.Run(factory)) { }
+        public bool IsValueCreated => _lazy.IsValueCreated;
+        public Task<T> Value => _lazy.Value;
+
+        public async ValueTask Reset()
+        {
+            if (this.IsValueCreated)
+            {
+                await DisposeLazy();
+                _lazy = new Lazy<Task<T>>(_factory);
+            }
+        }
+
+        private async ValueTask DisposeLazy()
+        {
+            if (!this.IsValueCreated) return;
+
+            var t = await this;
+            switch (t)
+            {
+                case IAsyncDisposable ad:
+                    await ad.DisposeAsync();
+                    break;
+                case IDisposable d:
+                    d.Dispose();
+                    break;
+            }
+
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeLazy();
+        }
+        public TaskAwaiter<T> GetAwaiter() => Value.GetAwaiter();
+    }
+}
